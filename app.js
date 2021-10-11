f999952887b001577375710e595016e594727652
Camilo A <ing_camiloacg@hotmail.com>
2021-10-11 16:43:54 -0500

    Ahora sí

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app = express();
 
 // Conexión base de datos
 const mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/Tienda_CA';
+// const uri = 'mongodb://localhost:27017/Tienda_CA';
+const uri = 'mongodb+srv://caDb:robomongo@cluster0.svtoq.mongodb.net/tienda_ca?retryWrites=true&w=majority';
 const options = {useNewUrlParser: true, useUnifiedTopology: true};
 
 mongoose.connect(uri, options).then(
